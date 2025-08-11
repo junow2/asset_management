6956468cfa576014f65d9c76418e91e4a4a6b661
junow2 <quilt257@gmail.com>
2025-08-11 14:22:52 +0900

    simplified employee

--- back/src/main/java/com/example/back/entity/Employee.java
+++ back/src/main/java/com/example/back/entity/Employee.java
@@ -39,35 +39,9 @@ public class Employee {
   private YearMonth joinDate;
 
   @Enumerated(EnumType.STRING)
-  // private Position position;     // 직급
+  private Position position;     // 직급
 
   private Integer paidMonths;    // 납부 개월 수 (수정 가능)
   private Integer paidAmount;    // 납부 금액 (수정 가능)
   private Integer unpaidAmount;  // 미납 금액 (계산값)
-
-  @PrePersist
-  @PreUpdate
-  public void calculateUnpaidAmount() {
-      int positionFee = getPositionFee(position);
-      int calculatedPaidMonths = (paidMonths != null) ? paidMonths : calcDefaultPaidMonths();
-      int calculatedUnpaid = (positionFee * calculatedPaidMonths) - (paidAmount != null ? paidAmount : 0);
-      this.unpaidAmount = Math.max(calculatedUnpaid, 0);
-  }
-  
-  private int calcDefaultPaidMonths() {
-      YearMonth now = YearMonth.now();
-      return (int) (joinDate.until(now, java.time.temporal.ChronoUnit.MONTHS) + 1);
-  }  
-
-  private int getPositionFee(String position) {
-    return switch (position) {
-        case "수석" -> 20000;
-        case "책임" -> 20000;
-        case "차장" -> 15000;
-        
-        default -> 10000; // 사원 등 
-    };
-  }
-
-
 }
